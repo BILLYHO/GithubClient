fd6f25dba26bad3a78da99f31ff06bf2e1756a03
hejiajun04 <hejiajun04@meituan.com>
2025-04-10 10:18:51 +0800

    Initial Commit

---
+++ GithubClient/GithubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubClientApp.swift
+//  GithubClient
+//
+//  Created by BILLY  HO on 2025/4/10.
+//
+
+import SwiftUI
+
+@main
+struct GithubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
